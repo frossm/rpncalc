6d04840470cd0e71c62b2c71467e2d53829758a1
Michael Fross <michael@fross.org>
2019-12-19 12:32:13 -0600

    Help Tweaks

--- src/main/java/org/fross/rpn/Help.java
+++ src/main/java/org/fross/rpn/Help.java
@@ -27,17 +27,16 @@ public class Help {
 	public static void Display() {
 		Output.printColorln(FColor.YELLOW,
 				"\n+----------------------------------------------------------------------+");
-		Output.printColorln(FColor.YELLOW,
-				"+              RPN Calculator v" + Main.VERSION + "  Help Document               +");
+		Output.printColorln(FColor.WHITE, "+              RPN Calculator v" + Main.VERSION + "  Help Document               +");
 		Output.printColorln(FColor.YELLOW, "+----------------------------------------------------------------------+");
-		Output.printColorln(FColor.WHITE, "            RPN is a simple reverse polish notation calculator");
-		Output.printColorln(FColor.WHITE, "                     https://github.com/frossm/rpn\n");
+		Output.printColorln(FColor.CYAN, "            RPN is a simple reverse polish notation calculator");
+		Output.printColorln(FColor.CYAN, "                     https://github.com/frossm/rpn");
 
-		Output.printColorln(FColor.YELLOW, "Command Line Options:");
+		Output.printColorln(FColor.YELLOW, "\nCommand Line Options:");
 		Output.printColorln(FColor.WHITE, " -l     Load saved named stack. Create the stack if it does not exist");
-		Output.printColorln(FColor.WHITE, " -D     Start in debug mode.  Same as using the 'debug' command\n");
+		Output.printColorln(FColor.WHITE, " -D     Start in debug mode.  Same as using the 'debug' command");
 
-		Output.printColorln(FColor.YELLOW, "Operands:");
+		Output.printColorln(FColor.YELLOW, "\nOperands:");
 		Output.printColorln(FColor.WHITE, " +      Addition:  Add last two stack elements");
 		Output.printColorln(FColor.WHITE, " -      Subtraction: Subtract last element from previous element");
 		Output.printColorln(FColor.WHITE, " *      Multiplication: Muliply last two stack items");
@@ -45,7 +44,7 @@ public class Help {
 		Output.printColorln(FColor.WHITE, " ^      Power:  Calculate second to last item to the power of the last item");
 		Output.printColorln(FColor.WHITE, " %      Percent: Turn the last stack item into a percentage (multiplied by 0.01)");
 
-		Output.printColorln(FColor.YELLOW, "Commands:");
+		Output.printColorln(FColor.YELLOW, "\nCommands:");
 		Output.printColorln(FColor.WHITE, " u         Undo.  Replaces current stack with one from before previous operation");
 		Output.printColorln(FColor.WHITE, " s[#] [#]  Swap the last two elments in the stack or the elements provided");
 		Output.printColorln(FColor.WHITE, " c         Clear everything from the visible stack");
