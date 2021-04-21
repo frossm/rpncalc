f6eb697864ff8f31bf37180f44ce56646387926c
Michael Fross <michael@fross.org>
2021-04-20 19:42:33 -0500

    Update help text

--- src/main/java/org/fross/rpncalc/Help.java
+++ src/main/java/org/fross/rpncalc/Help.java
@@ -59,14 +59,14 @@ public class Help {
 		Output.printColorln(Ansi.Color.WHITE, " -w num   Set Width of header / status line.  Default is 70 characters");
 		Output.printColorln(Ansi.Color.WHITE, " -v       Display version information as well as latest GitHub release");
 		Output.printColorln(Ansi.Color.WHITE, " -z       Disable colorized output");
-		Output.printColorln(Ansi.Color.WHITE, " h | ?    Show this help information.  Either key will work.");
+		Output.printColorln(Ansi.Color.WHITE, " -h | ?   Show this help information.  Either key will work.");
 
 		Output.printColorln(Ansi.Color.YELLOW, "\nOperands:");
 		Output.printColorln(Ansi.Color.WHITE, " +    Addition:  Add last two stack elements");
 		Output.printColorln(Ansi.Color.WHITE, " -    Subtraction: Subtract row 1 from row 2");
 		Output.printColorln(Ansi.Color.WHITE, " *    Multiplication: Muliply last two stack items");
-		Output.printColorln(Ansi.Color.WHITE, " /    Division: Divide row 2 by row 1");
-		Output.printColorln(Ansi.Color.WHITE, " ^    Power:  Calculate row 2 to the power of row 1");
+		Output.printColorln(Ansi.Color.WHITE, " /    Division: Divide line2 by line1");
+		Output.printColorln(Ansi.Color.WHITE, " ^    Power:  Calculate line2 to the power of line1");
 
 		Output.printColorln(Ansi.Color.YELLOW, "\nCalculator Commands:");
 		Output.printColorln(Ansi.Color.WHITE, " u            Undo last action");
@@ -74,16 +74,16 @@ public class Help {
 		Output.printColorln(Ansi.Color.WHITE, " c            Clear the screen and empty current stack");
 		Output.printColorln(Ansi.Color.WHITE, " clean        Clear screen but save the stack values");
 		Output.printColorln(Ansi.Color.WHITE, " d [#]        Delete the last item in the stack or the row number provided");
-		Output.printColorln(Ansi.Color.WHITE, " s [#] [#]    Swap the last two elments in the stack or the rows provided");
-		Output.printColorln(Ansi.Color.WHITE, " %            Convert last stack item into a percentage by multipling by 0.01");
+		Output.printColorln(Ansi.Color.WHITE, " s [#] [#]    Swap the last two elments in the stack or the lines provided");
+		Output.printColorln(Ansi.Color.WHITE, " %            Convert line1 into a percentage by multipling by 0.01");
 		Output.printColorln(Ansi.Color.WHITE, " sqrt         Perform a square root");
 		Output.printColorln(Ansi.Color.WHITE, " round [n]    Round to n decimal places.  Default is 0 decimals");
 		Output.printColorln(Ansi.Color.WHITE, " aa [keep]    Add all stack items. Adding Keep will keep added elements");
 		Output.printColorln(Ansi.Color.WHITE, " mod          Modulus. Perform a division and return the remainder");
 		Output.printColorln(Ansi.Color.WHITE, " copy         Copy the last stack item");
-		Output.printColorln(Ansi.Color.WHITE, " log|log10    Calculate the natural (base e) or base10 logarithm");
-		Output.printColorln(Ansi.Color.WHITE, " int          Convert top stack item to integer discarding decimal");
-		Output.printColorln(Ansi.Color.WHITE, " abs          Take the absolute value of top stack item");
+		Output.printColorln(Ansi.Color.WHITE, " log | log10  Calculate the natural (base e) or base10 logarithm");
+		Output.printColorln(Ansi.Color.WHITE, " int          Convert line1 to an integer discarding decimal");
+		Output.printColorln(Ansi.Color.WHITE, " abs          Take the absolute value of line1");
 		Output.printColorln(Ansi.Color.WHITE, " rand [L] [H] Random integer between X and Y inclusive.  Default is 1-100");
 		Output.printColorln(Ansi.Color.WHITE, " dice XdY     Roll a Y sided die X times.  Default is 1d6");
 
@@ -97,7 +97,7 @@ public class Help {
 		Output.printColorln(Ansi.Color.YELLOW, "\nTrigonometry Functions:");
 		Output.printColorln(Ansi.Color.WHITE, " sin|cos|tan [rad]    Trig Functions: Angle in degrees unless rad is provided");
 		Output.printColorln(Ansi.Color.WHITE, " asin|acos|atan [rad] Trig Functions: Result in degrees unless rad is provided");
-		Output.printColorln(Ansi.Color.WHITE, " hypot        Returns the hypotenuse using line1 and line2 as the legs");
+		Output.printColorln(Ansi.Color.WHITE, " hypot                Returns the hypotenuse using line1 and line2 as the legs");
 
 		Output.printColorln(Ansi.Color.YELLOW, "\nMemory Commands:");
 		Output.printColorln(Ansi.Color.WHITE, " mem [X] add   Add last stack item to memory Slot X. Default is 0");
@@ -121,11 +121,11 @@ public class Help {
 		Output.printColorln(Ansi.Color.WHITE, " debug        Toggle DEBUG mode on/off");
 		Output.printColorln(Ansi.Color.WHITE, " ver          Display the current version");
 		Output.printColorln(Ansi.Color.WHITE, " h|?          Show this help information.  Either key will work.");
-		Output.printColorln(Ansi.Color.WHITE, " x|exit       Exit Calculator.  'cx' will clear before exiting");
+		Output.printColorln(Ansi.Color.WHITE, " cx|x|exit    Exit Calculator.  'cx' will clear before exiting");
 
 		Output.printColorln(Ansi.Color.YELLOW, "\nNotes:");
 		Output.printColorln(Ansi.Color.WHITE, "  - You can place an operand at the end of a number & execute in one step.");
 		Output.printColorln(Ansi.Color.WHITE, "    Example adding two numbers:   2 <enter> 3+ <enter>   will produce 5.");
-		Output.printColorln(Ansi.Color.CYAN, "  - See GitHub homepage (listed above) for additional usage instructions\n");
+		Output.printColorln(Ansi.Color.CYAN, "  - See GitHub homepage (listed above) for more detailed usage instructions\n");
 	}
 }
